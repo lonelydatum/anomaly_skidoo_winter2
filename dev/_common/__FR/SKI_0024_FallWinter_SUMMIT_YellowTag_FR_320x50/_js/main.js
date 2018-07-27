68d710d56c32302c1c5955bdf20ad8c52982a48d
Gar <gar@lonelydatu.com>
2018-07-27 18:01:15 -0400

    sdfdf

---
+++ dev/_common/__FR/SKI_0024_FallWinter_SUMMIT_YellowTag_FR_320x50/_js/main.js
@@ -0,0 +1,3 @@
+import start from '../../_common/js/heliumOnes'
+TweenMax.to("#tag", 1.7, {skewX:"+=1", skewY:"+=1", rotation:"+=2", scale:.49,  repeat:3, yoyo:true, ease:Power1.easeInOut})
+start(.03);
